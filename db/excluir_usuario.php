35bf1a80c5d837198e68d8309365630ba5edd33e
Abrahão Eneias <abrahao.eneias@MacBook-Air.local>
2020-02-04 15:50:55 -0300

    Padronizacao da tela Excluir Usuario

--- db/excluir_usuario.php
+++ db/excluir_usuario.php
@@ -1,29 +1,4 @@
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-    <link rel="stylesheet" href="./Academia-PHP/style.css">
-    <title>Excluir Registro</title>
-
-
-    <br>
-    <div class="barra-voltar">
-        <div class="row">
-            <div class="col">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="../index.php">Home</a></li>
-                        <li class="breadcrumb-item"><a href="../menu-admin.php">Menu Administrativo</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Adicionar Usuário</li>
-                    </ol>
-                </nav>
-            </div>
-        </div>
-    </div>
-    <br>
-
-    <?php
+<?php
     require_once "conexao.php";
 
     $registros = [];
@@ -49,6 +24,41 @@
     $conexao->close;
     ?>
 
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link rel="stylesheet" href="../style.css">
+    <title>Administrativo</title>
+</head>
+
+<body>
+
+<?php
+    require_once "../cabecalho.php";
+?>
+
+    <br>
+    <div class="barra-voltar">
+        <div class="row">
+            <div class="col">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="../index.php">Home</a></li>
+                        <li class="breadcrumb-item"><a href="../menu-admin.php">Menu Administrativo</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Excluir Usuário</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
+    </div>
+
     <table class="table table-hover table-striped table-bordered">
         <thead>
             <th>ID</th>
@@ -61,7 +71,7 @@
                     <td><?= $registro['id'] ?></td>
                     <td><?= $registro['nome'] ?></td>
                     <td>
-                    <a href="excluir_usuario=<?= $registro['id'] ?>" 
+                    <a href="#=<?= $registro['id'] ?>" 
                         class="btn btn-danger">
                         Excluir
                     </a>
@@ -76,3 +86,7 @@
             font-size: 1.2rem;
         }
     </style>
+
+</body>
+
+</html>
